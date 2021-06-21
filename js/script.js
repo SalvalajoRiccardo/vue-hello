f12a3c020e223b43fc95626b391f93846f72d0aa
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-21 15:30:25 +0200

    fine + bonus

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+var app = new Vue({
+    
+    el: '#app',
+    data: {
+        titolo : 'hello vue',
+        // bonus
+        image : 'https://images-na.ssl-images-amazon.com/images/I/91M-sgH80-L._AC_SL1500_.jpg'
+    }
+});
